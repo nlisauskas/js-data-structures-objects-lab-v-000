9e71de01e949cc561533ebc8003fd2d0b202566b
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-03-12 13:11:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Write your solution in this file!
 const driver = {
-  
+
 }
 
 function updateDriverWithKeyAndValue(driver, key, value) {
   driver[key] = value;
 
-  return driver
+  return driver;
 }
